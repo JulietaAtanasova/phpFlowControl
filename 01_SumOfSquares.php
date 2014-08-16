acd6c02fe203f9cc4ae9740d6e6347422a46369d
Dsmilyanov <dsmilyanov@gmail.com>
2014-08-16 03:34:39 +0300

    finalized document

--- 01_SumOfSquares.php
+++ 01_SumOfSquares.php
@@ -10,11 +10,11 @@ echo "<table border=\"1\">";
 echo "<tr><th>Number</th>";
 echo "<th>Square</th></tr>";
 $sum = 0;
-for ( $number = 0; $number <= 100; $number+=2){
+for ( $number = 0; $number <= 40; $number+=2){
     $square = round(sqrt($number), 2);
     $sum += $square;
     echo "<tr><td>" . $number . "</td><td>" . $square . "</td></tr>";
-    if ($number == 100){
+    if ($number == 40){
         echo "<tr><td><strong>Total: </strong></td><td>" . $sum . "</td></tr>";
     }
 }
